a1745d5fa9ebcae994521f3c8c03f1782b8fada4
Ronald Rey <reyronald@gmail.com>
2026-01-08 04:34:23 -0400

    useFormContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
 import * as React from "react";
-import { useForm } from "react-hook-form";
+import {
+  useForm,
+  useWatch,
+  useFormContext,
+  FormProvider,
+} from "react-hook-form";
 import Headers from "./Header";
 import "./styles.css";
 
 export default function App() {
-  const { register, handleSubmit, watch } = useForm();
+  const methods = useForm();
+  const { register, handleSubmit, watch, control } = methods;
   const onSubmit = (data) => console.log(data);
+
+  const test = useWatch({ control, name: "test" });
 
   return (
     <div>
-      <Headers
-        description="Performant, flexible and extensible forms with easy-to-use validation."
-      />
+      <Headers description="Performant, flexible and extensible forms with easy-to-use validation." />
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register("test")} placeholder="Test field" />
-        Value is {watch("test")}
-        <input type="submit" />
-      </form>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("test")} placeholder="Test field" />
+
+          <div>useForm.watch is {watch("test")}</div>
+
+          <div>useForm.useWatch value is {test}</div>
+
+          <Component />
+
+          <input type="submit" />
+        </form>
+      </FormProvider>
     </div>
+  );
+}
+
+function Component() {
+  const { watch, control } = useFormContext();
+
+  const test = useWatch({ control, name: "test" });
+
+  return (
+    <>
+      <div>useFormContext.watch is {watch("test")}</div>
+
+      <div>useForm.useWatch value is {test}</div>
+    </>
   );
 }
